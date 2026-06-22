fb7b720c97ffe8972ebef343b7c9fe99491414fc
aikerim <aikerim.879@gmail.com>
2026-06-23 01:46:06 +0600

    init commit

--- src/test/java/pages/CaterpillarJobsPage.java
+++ src/test/java/pages/CaterpillarJobsPage.java
@@ -1,4 +1,44 @@
 package pages;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.*;
+
 public class CaterpillarJobsPage {
+
+    private final SelenideElement searchInput = $("#l-search");
+    private final SelenideElement searchButton = $("#js-main-job-search");
+    private final SelenideElement resultsContainer = $("#js-job-search-results");
+    private final ElementsCollection jobCards = $$(".card-job");
+
+    public CaterpillarJobsPage open() {
+        com.codeborne.selenide.Selenide.open("https://careers.caterpillar.com/en/jobs/?search=&team=Technology%2C+Digital+and+Data&team=Engineering#results");
+        return this;
+    }
+
+    public CaterpillarJobsPage closeCookieBanner() {
+        SelenideElement cookieBtn = $("[id*='cookie'] button, .js-accept-cookies, #onetrust-accept-btn-handler");
+        if (cookieBtn.exists()) {
+            cookieBtn.click();
+        }
+        return this;
+    }
+
+    public CaterpillarJobsPage search(String query) {
+        searchInput.setValue(query);
+        searchButton.click();
+        return this;
+    }
+
+    public int getJobCount() {
+        return Integer.parseInt(resultsContainer.attr("data-results"));
+    }
+
+    public int getVisibleCardCount() {
+        return jobCards.size();
+    }
+
+    public String getFirstJobTitle() {
+        return jobCards.first().$(".js-view-job").getText();
+    }
 }
--- src/test/java/tests/CaterpillarJobsTest.java
+++ src/test/java/tests/CaterpillarJobsTest.java
@@ -1,4 +1,43 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import pages.CaterpillarJobsPage;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaterpillarJobsTest {
+
+    private final CaterpillarJobsPage jobsPage = new CaterpillarJobsPage();
+
+    @Test
+    void pageOpensAndShowsJobs() {
+        jobsPage.open();
+        assertTrue(jobsPage.getJobCount() > 0);
+    }
+
+    @Test
+    void searchReturnsResults() {
+        jobsPage.open();
+        jobsPage.closeCookieBanner();
+        jobsPage.search("Engineer");
+        assertTrue(jobsPage.getJobCount() > 0);
+    }
+
+    @Test
+    void pageShowsTwentyCards() {
+        jobsPage.open();
+        assertEquals(20, jobsPage.getVisibleCardCount());
+    }
+
+    @Test
+    void firstJobTitleIsNotEmpty() {
+        jobsPage.open();
+        assertFalse(jobsPage.getFirstJobTitle().isEmpty());
+    }
+
+    @Test
+    void jobCountIsPositiveNumber() {
+        jobsPage.open();
+        int count = jobsPage.getJobCount();
+        assertTrue(count > 100);
+    }
 }
